1d17cfd49830db2e0cbcb5d3a2e436da5dd9d395
Eduardo <eduhmq@gmail.com>
2022-09-28 23:52:26 -0300

    feito exercicio 4

---
+++ modulo1/lista1/Exercicio4/src/Exercicio4.java
@@ -0,0 +1,13 @@
+public class Exercicio4 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int x = 0;
+        x = a;
+        a = b;
+        b = x;
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
